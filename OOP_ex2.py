c7a1a329961bdd9e68cd1db2fc006b9c1f99a4b0
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-10-05 09:09:55 +0300

    Update OOP_ex2.py

--- OOP_ex2.py
+++ OOP_ex2.py
@@ -1,1 +1,17 @@
 
+from math import gcd
+
+class Rational:
+    def __init__(self, numerator = 1, denominator = 2):
+        self.__numerator = numerator / gcd(numerator, denominator);
+        self.__denominator = denominator / gcd(numerator, denominator);
+    def displayStandart(self):
+        print(f'{int(self.__numerator)}/{int(self.__denominator)}');
+    def displayFloating(self):
+        print(f'{self.__numerator / self.__denominator}');
+
+x = Rational(10,20);
+
+x.displayStandart();
+
+x.displayFloating();
