0555f63064bd55a942f7d9d6089896f951f47ed8
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-11-01 17:07:48 +0200

    Update Group.py

--- Group.py
+++ Group.py
@@ -1,1 +1,17 @@
+class Student:
+    classes = {};
+    def __init__(self, name, surname, RBN):
+        self.name = name;
+        self.surname = surname;
+        self.RBN = RBN;
+    def gradeSetter(self, subject, grade):
+        if subject not in self.classes:
+            self.classes[subject] = [];
+        self.classes[subject].append(grade);
 
+x = Student("Name", "Surname", 1488);
+x.gradeSetter('Math', 88);
+x.gradeSetter('Math', 143);
+x.gradeSetter('Arts', 98);
+x.gradeSetter('Arts', 43);
+print(x.classes);
