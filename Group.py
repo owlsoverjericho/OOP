7080732cdfa8ba4d71fa81c8af3654ae92d5ae9f
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-11-01 17:20:56 +0200

    Update Group.py

--- Group.py
+++ Group.py
@@ -8,10 +8,18 @@ class Student:
         if subject not in self.classes:
             self.classes[subject] = [];
         self.classes[subject].append(grade);
-
+    def averageClass(self, Class):
+        sum = 0;
+        for grade in self.classes[Class]:
+            sum += grade;
+        return sum / len(self.classes[Class]);
 x = Student("Name", "Surname", 1488);
+
 x.gradeSetter('Math', 88);
 x.gradeSetter('Math', 143);
 x.gradeSetter('Arts', 98);
 x.gradeSetter('Arts', 43);
+
 print(x.classes);
+
+print(x.averageClass('Math'));
