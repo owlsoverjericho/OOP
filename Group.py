4ab14e894b01bfab750ef52e41d38c3aa8c459f6
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-11-02 13:36:28 +0200

    Update Group.py

--- Group.py
+++ Group.py
@@ -1,9 +1,21 @@
+class Group:
+    def __init__(self, groupName, academicDepartment, faculty):
+        self.groupName = groupName;
+        self.academicDepartment = academicDepartment;
+        self.faculty = faculty;
+        self.students = [];
+    def addStudent(self, Student):
+        for student in self.students:
+            if Student.RBN == student.RBN:
+                return "Already added!";
+            self.students.append(Student);
+    
 class Student:
-    classes = {};
     def __init__(self, name, surname, RBN):
         self.name = name;
         self.surname = surname;
         self.RBN = RBN;
+        self.classes = {};
     def gradeSetter(self, subject, grade):
         if subject not in self.classes:
             self.classes[subject] = [];
@@ -13,13 +25,14 @@ class Student:
         for grade in self.classes[Class]:
             sum += grade;
         return sum / len(self.classes[Class]);
-x = Student("Name", "Surname", 1488);
-
-x.gradeSetter('Math', 88);
-x.gradeSetter('Math', 143);
-x.gradeSetter('Arts', 98);
-x.gradeSetter('Arts', 43);
-
-print(x.classes);
-
-print(x.averageClass('Math'));
+        
+x = Student("Name1", "Surname1", 1);
+y = Student("Name1", "Surname1", 2);
+z = Student("Name1", "Surname1", 3);
+w = Student("Name1", "Surname1", 4);
+TV = Group("TV-z01", "APEPS", "TEF");
+TV.addStudent(x);
+TV.addStudent(y);
+TV.addStudent(z);
+TV.addStudent(w);
+TV.addStudent(w);
