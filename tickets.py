afc9a7ba170415f12fbe355e8e048d01524caec7
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-11-08 20:04:54 +0200

    Create tickets.py

---
+++ tickets.py
@@ -0,0 +1,9 @@
+Write a program for selling tickets to IT-events. Each ticket has a unique number and a price. There are four types of tickets: regular ticket, advance ticket (purchased 60 or more days before the event), late ticket (purchased fewer than 10 days before the event) and student ticket.
+Additional information:
+-advance ticket - discount 40% of the regular ticket price;
+-student ticket - discount 50% of the regular ticket price;
+-late ticket - additional 10% to the reguler ticket price.
+All tickets must have the following properties:
+-the ability to construct a ticket by number;
+-the ability to ask for a ticket’s price;
+-the ability to print a ticket as a String.
