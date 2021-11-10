3f2fdde86d44144c29cd8a25679156058a27db63
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-11-10 22:41:50 +0200

    Create pizza.py

---
+++ pizza.py
@@ -0,0 +1,6 @@
+#Pizzeria offers pizza-of-the-day for business lunch. 
+#The type of pizza-of-the-day depends on the day of week. 
+#Having a pizza-of-the-day simplifies ordering for customers. 
+#They don't have to be experts on specific types of pizza. 
+#Also, customers can add extra ingredients to the pizza-of-the-day. 
+#Write a program that will form orders from customers.
