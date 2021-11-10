cdfef3bd6396a05f960536d6bd57fb31a7e975b2
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-11-10 17:01:13 +0200

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -2,8 +2,6 @@
 #-student ticket - discount 50% of the regular ticket price;
 #-late ticket - additional 10% to the reguler ticket price
 
-import random
-
 class Ticket:
     def __init__(self, price, number):
         self.price = price
@@ -14,40 +12,49 @@ class TicketFactory:
         self.regular = basePrice
         self.advanced = int(basePrice * 0.4)
         self.student = int(basePrice * 0.5)
-        self.atTheDay = int(basePrice * 1.1)
+        self.late = int(basePrice * 1.1)
+        #lists of integer numbers for each ticket type
         self.regularNumbers = list(range(100, 500))
         self.advancedNumbers = list(range(501, 1000))
         self.studentNumbers = list(range(1001, 1500))
-        self.atTheDayNumbers = list(range(1501, 2000))
-        self.regularPool = []
-        self.advancedPool = []
-        self.studentPool = []
-        self.atTheDayPool = []
+        self.lateNumbers = list(range(1501, 2000))
+        #these store tickets (instances of Ticket class)
+        self.regularTickets = []
+        self.advancedTickets = []
+        self.studentTickets = []
+        self.lateTickets = []
     def generateRegular(self, amount):
         for number in range(amount):
-            self.regularPool.append(Ticket(self.regular, self.regularNumbers[number]))
+            self.regularTickets.append(Ticket(self.regular, self.regularNumbers[number]))
     def generateAdvanced(self, amount):
         for number in range(amount):
-            self.advancedPool.append(Ticket(self.advanced,  self.advancedNumbers[number]))
+            self.advancedTickets.append(Ticket(self.advanced,  self.advancedNumbers[number]))
     def generateStudent(self, amount):
         for number in range(amount):
-            self.studentPool.append(Ticket(self.student,  self.studentNumbers[number]))
-    def generateAtTheDay(self, amount):
+            self.studentTickets.append(Ticket(self.student,  self.studentNumbers[number]))
+    def generateLate(self, amount):
         for number in range(amount):
-            self.atTheDayPool.append(Ticket(self.atTheDay, self.atTheDayNumbers[number]))
-            
-x = TicketFactory(131);
+            self.lateTickets.append(Ticket(self.late, self.lateNumbers[number]))
 
-x.generateRegular(10)
+def EventCreator(daysBeforeEvent, regularTickets):
+    event = TicketFactory(60)
+    event.generateRegular(regularTickets)
+    event.generateStudent(int(regularTickets * 0.3))
+    if daysBeforeEvent >= 60:
+        event.generateAdvanced(int(regularTickets * 0.1))
+        event.generateLate(0)
+    if daysBeforeEvent < 10:
+        event.generateAdvanced(0)
+        event.generateLate(int(regularTickets * 0.22))
+    return event
 
-x.generateAdvanced(10)
+def availableTickets(event):
+    
+    
 
-x.generateStudent(10)
-
-x.generateAtTheDay(10)
-
-print(f"Regular ticket price: {x.regularPool[0].price}, tickets available {len(x.regularPool)}")
-
-print(f"Advanced ticket price: {x.advancedPool[0].price}, tickets available {len(x.advancedPool)}")
-print(f"Student ticket price: {x.studentPool[0].price}, tickets available {len(x.studentPool)}",)
-print(f"At the day ticket price: {x.atTheDayPool[0].price}, tickets available {len(x.atTheDayPool)}")
+while True:
+    event = EventCreator(60, 100)
+    
+    
+    break
+    
