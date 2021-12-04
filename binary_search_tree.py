f3bfdc487dcbfaf7320545f7997827f0b7af58c0
owlsoverjericho <46383132+owlsoverjericho@users.noreply.github.com>
2021-12-04 17:06:36 +0200

    Update binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,26 +1,6 @@
-class Node:
-    def __init__(self, data, result):
-        self.data = data;
-        self.result = result;
-        self.left = None;
-        self.right = None;
-        
-root = Node(2, 100);
-
-root.left = Node(1, 101);
-
-root.right = Node(3, 102);
-root.right.right = Node(4, 103);
-
-def search(node, value):
-    if node != None:
-        if node.data == value:
-            print("done", node.result);
-        if value < node.data:
-            search(node.left, value);
-        if value > node.data:
-            search(node.right, value);
-    else:
-        print("Search unsuccessful");
-        
-search(root, 4);
+#Modify the class Rational of Lab No2 to perform the following tasks:
+#- adding two Rational numbers. The result should be stored in reduced form;
+#- subtracting two Rational numbers. The result should be stored in reduced form;
+#- multiplying two Rational numbers. The result should be stored in reduced form;
+#- dividing two Rational numbers. The result should be stored in reduced form;
+#- comparison two Rational numbers.
